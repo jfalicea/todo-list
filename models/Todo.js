44639f8ec407ed0c39529f510ac45d07e48139b7
Jonathan <jonathan@boldideas.net>
2019-08-13 14:49:26 -0400

    puts functions into models.

---
+++ models/Todo.js
@@ -0,0 +1,44 @@
+const db = require('../db')    
+    
+    //using pg-promise ONE and ONLY ONE.  RETURNS a single object. it'll error if: there is 0 items or MORE than 1.   This is good for getting unique items. 
+function getOne(id){    
+    db.one(`
+        SELECT * FROM todos
+        WHERE id = $1                  
+        `,[id]
+    )
+            .then((data)=>{  //THEN get the data back from the query. 
+                console.log(`here is the data:`);
+                console.log(data);  //pass the variable into to get the data!
+            })       
+            .catch((err)=>{
+                console.log('err');
+                console.log("err Object: ", err)
+                console.log("error message: ",err.message);
+                console.log("error query:", err.query);
+            })
+    }
+
+ //SQL Query must be a string.  #pg-promise ANY RETURNS AN ARRAY OF OBJECTS. 
+ function getAll(){
+    db.any(`
+    SELECT * FROM todos;
+    `)
+        .then((data)=>{       //THEN get the data back from the query. 
+            console.log(`here is the data:`);
+            console.log(data);  //pass the variable into to get the data!
+        })       
+        .catch((err)=>{
+                console.log('err');
+                console.log(err);
+        })
+}
+
+module.exports={
+//if the key name is the same as the value you can just use the key. 
+//getAll : getAll ===>  this is called enhanced object literalsyntax. 
+    getAll, 
+    getOne,
+}
+
+    
