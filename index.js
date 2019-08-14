8d7ceea08a17cc526ea48b5d36dbbac48ee4ed6a
Jonathan <jonathan@boldideas.net>
2019-08-14 10:10:52 -0400

    adds express server

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
-const http = require(`http`);
-const Todo = require('./models/Todo')
+/*------------------------------------------
+* ORIGINAL Way to handle HTTP Requests
+------------------------------------------*/
+// const http = require(`http`);
+// const Todo = require('./models/Todo')
 
 
-
-const server = http.createServer((request, response)=>{
-    const allTodos = Todo.getAll()
+/*------------------------------------------
+HOW TO CREATE A SERVER WITHOUT EXPRESS. 
+------------------------------------------*/
+// const server = http.createServer((request, response)=>{
+//     const allTodos = Todo.getAll()
     
+//     allTodos.then((data)=>{ 
+//         let jsonData = JSON.stringify(data[0].task)
+//         response.end("Task: "+jsonData);
+//     });
+// });
+// server.listen(3000); 
+
+/*------------------------------------------
+* HOW TO USE EXPRESS.js
+------------------------------------------*/
+const express = require('express'); // replace http with 'express'
+const Todo = require('./models/Todo')
+const app = express();    //create the server and call it "app"  
+const port = 3000;  //variable for the port number. 
+
+app.get('/todos',(request, response)=>{
+    const allTodos = Todo.getAll()
     allTodos.then((data)=>{ 
-        let jsonData = JSON.stringify(data[0].task)
-        response.end("Task: "+jsonData);
-    });
-});
-server.listen(3000); 
+        // let jsonData = JSON.stringify(data);
+        // response.end("Task: "+jsonData);
+        response.json(data);
+        })});
+app.listen(port);
+
+
 
 
